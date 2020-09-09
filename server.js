b3b2793f7fbe2a58bac8dada12ddd5574e15e013
Geovane Borba <geovanne@live.com>
2020-09-08 21:09:26 -0300

    chore: add template engine nunjucks

--- server.js
+++ server.js
@@ -7,5 +7,12 @@ const app = express()
 app.use(cors())
 app.use(express.static("public"))
 
+// Configuração do template engine
+app.set("view engine", "html")
+
+nunjucks.configure("views", {
+    express: app
+})
+
 
 app.listen(3001, () => console.log('Servidor rodando na porta 3001'))
